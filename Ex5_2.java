a0b69fa78afe5aa28e499695c38dc2fabde040d3
ji200407 <ji200407@naver.com>
2024-04-12 12:48:51 +0900

    4월12일
    내용정리

---
+++ Ex5_2.java
@@ -0,0 +1,25 @@
+public class Ex5_2 {
+    public static void main(String[] args) {
+
+        Color
+        cp.set();
+        cp.showPoint();
+        cp.setColor("red");
+        cp.showColorPoint();
+    }
+    
+}
+
+class Point{
+    private int x,y;
+    public Point(){
+        this.x=this.y=0;
+    }
+    public Point(int x,int y) {
+        this.x
+    }
+}
+class ColorPoin extends Point{
+    private String color;
+
+}
